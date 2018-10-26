c084117e4e08f7a150a70338ddd82e8ae4ed6ae7
Microsocks <barclayethan@gmail.com>
2018-10-26 16:35:17 -0400

    Added combine file functionality
    Also added progress bars and cleaned up the code as well as prompting the user to choose if they want to split or combine a file at run.

--- BasicFileSplit.cs
+++ BasicFileSplit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,25 +10,74 @@ namespace fileSplit
 {
     class BasicFileSplit
     {
+        public static int BUFFER_SIZE { get; private set; }
+
         static void Main(string[] args)
+        {
+            Console.WriteLine("Split File (1) or Combine Files (2)?");
+            string splitOrCombineInput = Console.ReadLine();
+            Console.Clear();
+            newInput:
+            int splitOrCombine = 0;
+            Int32.TryParse(splitOrCombineInput, out splitOrCombine);
+            if (splitOrCombine == 1)
+            {
+                PromptToSplit();
+            }
+            else if (splitOrCombine == 2)
+            {
+                PromptToCombine();
+            } else
+            {
+                Console.WriteLine("Incorrect input. Please enter either '1', or '2'.");
+                splitOrCombineInput = Console.ReadLine();
+                Console.Clear();
+                goto newInput;
+
+            }
+
+
+        }
+        public static void PromptToSplit()
         {
             Console.WriteLine("Name of File to split?");
             string fileName = Console.ReadLine();
+            Console.Clear();
             Console.WriteLine("Size of split files? (in MB)");
             string fileSizeInput = Console.ReadLine();
+            Console.Clear();
             int fileSizeParsed = 0;
             Int32.TryParse(fileSizeInput, out fileSizeParsed);
             fileSizeParsed = fileSizeParsed * 1000000; // 1000000 for MB, 1000 for KB
             Console.WriteLine("Output folder path?");
             string folderPath = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("File splitting of " + fileName + " to " + folderPath + " at " + fileSizeInput + " MB has begun.");
             SplitFile(fileName, fileSizeParsed, folderPath);
-            Console.WriteLine("File Splitting Complete");
+            Console.WriteLine("Files split successfully.");
+        }
+
+        public static void PromptToCombine()
+        {
+            Console.WriteLine("Folder of files to combine path?");
+            string folderPath = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Name of output file?");
+            string outputFileName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("File combination of " + folderPath + " to " + outputFileName + " has begun.");
+            CombineFile(folderPath, "*", outputFileName);
+            Console.WriteLine("Files combined successfully.");
         }
 
         public static void SplitFile(string inputFile, int chunkSize, string path)
         {
+
             const int BUFFER_SIZE = 20 * 1024;
             byte[] buffer = new byte[BUFFER_SIZE];
+            FileInfo f = new FileInfo(inputFile);
+            int inputFileSize = (int)f.Length;
+            int totalFiles = inputFileSize / chunkSize;
 
             using (Stream input = File.OpenRead(inputFile))
             {
@@ -47,8 +96,66 @@ namespace fileSplit
                     }
                     index++;
                     Thread.Sleep(500); // buffer to help other processes get IO
+                    drawTextProgressBar(index, totalFiles);
+
                 }
             }
         }
+
+        private static void CombineFile(string inputDirectoryPath, string inputFileNamePattern, string outputFilePath)
+        {
+            string[] inputFilePaths = Directory.GetFiles(inputDirectoryPath, inputFileNamePattern);
+            Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
+            int fileBeingProcessed = 1;
+            using (var outputStream = File.Create(outputFilePath))
+            {
+                foreach (var inputFilePath in inputFilePaths)
+                {
+                    using (var inputStream = File.OpenRead(inputFilePath))
+                    {
+                        // Buffer size can be passed as the second argument.
+                        inputStream.CopyTo(outputStream);
+                    }
+                    // Console.WriteLine("The file {0} has been processed.", inputFilePath);
+                    drawTextProgressBar(fileBeingProcessed, inputFilePaths.Length);
+                    fileBeingProcessed++;
+                }
+            }
+        }
+
+        private static void drawTextProgressBar(int progress, int total)
+        {
+            //draw empty progress bar
+            Console.CursorLeft = 0;
+            Console.Write("["); //start
+            Console.CursorLeft = 32;
+            Console.Write("]"); //end
+            Console.CursorLeft = 1;
+            float onechunk = 30.0f / total;
+
+            //draw filled part
+            int position = 1;
+            for (int i = 0; i < onechunk * progress; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw unfilled part
+            for (int i = position; i <= 31; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.CursorLeft = position++;
+                Console.Write("-");
+            }
+
+            //draw totals
+            Console.CursorLeft = 35;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
+        }
     }
+    
 }
+
